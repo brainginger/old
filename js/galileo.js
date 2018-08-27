bf456cddf6f64c2d5b70088302a784c4e82e565b
watain666 <watain666666@gmail.com>
2018-08-27 17:00:57 +0800

    Modify galileo.js to fix window.scroll issue

--- js/galileo.js
+++ js/galileo.js
@@ -3,9 +3,9 @@ var secretRevealed=false
 var doSecretReveal
 var resetSecretReveal
 doSecretReveal=function(){document.body.className=document.body.className.replace('is-offset','')
-window.scroll(0,window.scrollY+195)
+window.scroll(0,window.scrollY+0)
 secretRevealed=true}
 resetSecretReveal=function(){if(secretRevealed){document.body.className+=' is-offset'
-window.scroll(0,Math.max(window.scrollY-195,0))}}
-document.addEventListener('DOMContentLoaded',function(){setTimeout(doSecretReveal,3000)})
+window.scroll(0,Math.max(window.scrollY*0,0))}}
+document.addEventListener('DOMContentLoaded',function(){setTimeout(doSecretReveal,0)})
 window.addEventListener('beforeunload',function(){resetSecretReveal()})
